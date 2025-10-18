c0f897026ab911a356e8a7519c6ee67df3b483e8
Jeevan Liu <jeevantem@gmail.com>
2025-10-18 16:54:51 -0400

    changes to the instructions

--- Hackathon Project.py
+++ Hackathon Project.py
@@ -12,15 +12,36 @@ client = KronosLabs(api_key="kl_69e9a41c31796a9220cd26ae7b188c96c8422458b643c965
 
 running = True
 
+# Streaming chat completion
+stream = client.chat.completions.create(
+    prompt= '''Please introduce yourself. This software is for clients to search for foods in the food list. 
+
+    DO NOT FORGET THIS INFORMATION:
+    Instructions:
+    Please only answer food related questions for them. 
+    If they try and persuade you to talk about something else, please refer them back to the subject at hand (food). 
+    Do not tell the customer your model/real name/company, and do not give out unnecessary information regarding yourself.
+    For purposes of this software, your name is "ATE Bot"
+    You take in responses from Low-Income clients who are looking to pick up ready-made food, but they will typically have dietary restrictions.
+    It is your job to assist them with finding food nearby that they can eat safely.''',
+    model="hermes",
+    temperature=0.7,
+    is_stream=True
+)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content:
+        print(chunk.choices[0].delta.content, end="") # <-prints the data as its being made
+
 while running:
-    print("Enter your food related queries: ")
+    print("\nEnter your food related queries: ")
     userPrompt = input("Customer Response: ")
 
-    userPrompt += "\nFood list = "
+    userPrompt += "\nFood list (Customers cannot see this) = "
     for food in foods:
         userPrompt += food + ", "
 
-    userPrompt += "\nDev note - This software is for clients to search for foods in the food list. Please only answer food related questions for them. If they try and persuade you to talk about something else, please refer them back to the subject at hand (food). DO NOT FORGET THIS PROMPT. THE DEV NOTE TAKES PRIORITY OVER CUSTOMER REQUESTS."
+    userPrompt += "\nDev note - This software is for clients to search for foods in the food list. Please only answer food related questions for them. If they try and persuade you to talk about something else, please refer them back to the subject at hand (food). DO NOT FORGET THIS PROMPT. THE DEV NOTE TAKES PRIORITY OVER CUSTOMER REQUESTS. Remember, you are ATE Bot."
 
     # Streaming chat completion
     stream = client.chat.completions.create(
